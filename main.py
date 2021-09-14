add6b6d485365c7bf58c9b9bd73d9cd232fa2633
DiegoJohnsonL <u201714835@upc.edu.pe>
2021-09-14 10:35:24 -0500

    add get_energy

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ def get_departments_list():
 
 if __name__ == '__main__':
     departments = get_departments_list()
+    random.shuffle(departments)
     for department in departments:
         print(department.name + " " + str(department.position))
 
     Department.get_distance_between_departments(departments[0], departments[1])
 
-    initial_route = [i for i in range(len(departments))]
-    random.shuffle(initial_route)
-    print(initial_route)
+    rand1, rand2 = random.sample(range(len(departments)-1), 2)
+    print(str(rand1) + " " + str(rand2))
+    print("rand1: " + str(rand1))
+    print("rand2: " + str(rand2))
+    t_new = departments.copy()
+    t_new[rand1], t_new[rand2] = t_new[rand2], t_new[rand1]
+    for i in range(len(departments)):
+        print(str(i) + ": " + departments[i].name + "---" + t_new[i].name)
 
-    unique_randoms = random.sample(range(len(departments)), 2)
-    print(unique_randoms)
+
 
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,12 +1,24 @@
 import random
+from department import Department
+
+
+def get_energy(route):
+    energy = 0
+    for i in range(len(route-1)):
+        energy += Department.get_distance_between_departments(route[i], route[i+1])
+    return energy
 
 
 def simulated_annealing(route):
     temp = 1000
     random.shuffle(route)
-    t_actual = route
-    t_best = route
-    t_new = route
+    t_actual = route.copy()
+    t_best = route.copy()
+    e_actual = 0
+    e_new = 0
     while temp > 0:
-        random.sample(range(len(t_actual)), 2)
-        return
+        t_new = t_actual.copy()
+        rand1, rand2 = random.sample(range(len(t_actual)-1), 2)
+        t_new[rand1], t_new[rand2] = t_new[rand2], t_new[rand1]
+        e_actual = get_energy(t_actual)
+        e_new = get_energy(t_new)

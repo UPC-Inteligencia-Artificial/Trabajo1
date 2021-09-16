bac24cc4b71a87643d69c361eb9c41dcd66ab9f1
DiegoJohnsonL <u201714835@upc.edu.pe>
2021-09-16 15:12:36 -0500

    :)

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import time
+import folium
 import pandas as pd
-import random
 from department import Department
 from simulated_annealing import simulated_annealing
-import folium
-import pygame
-from selenium import webdriver
+
 
 def get_departments_list():
     departments_data = pd.read_csv("data/departamentos.csv")
@@ -16,60 +15,34 @@ def get_departments_list():
     return departments_list
 
 
-if __name__ == '__main__':
-    departments = get_departments_list()
-    best_route = simulated_annealing(departments, 10)
-    for route in best_route:
-        print(route.name + " " + str(route.position))
-
-    peru_map = folium.Map(location=[-9.497415, -75.142212], zoom_start=6)
-    for i in range(len(best_route)):
-        # iconSpy = folium.features.CustomIcon('icon.png')
-        # popupSpy = "<strong>Spy</strong><br>HOlA<br>"
-        # folium.Marker(location=city.position, tooltip="Spy", popup=popupSpy, icon=iconSpy).add_to(peru_map)
+def create_map(route):
+    peru_map = folium.Map(location=[-9.497415, -75.142212], zoom_start=6, zoom_control=False)
+    for i in range(len(route)):
         folium.Circle(
-            best_route[i].position,
+            route[i].position,
             radius=10000,
-            popup=best_route[i].name,
+            popup=route[i].name,
             color="crimson",
             fill=True,
         ).add_to(peru_map)
-        if i < len(best_route) -1:
+        if i < len(route) - 1:
             folium.PolyLine(
-                [best_route[i].position, best_route[i+1].position],
+                [route[i].position, route[i + 1].position],
                 color="blue",
                 weight=2.5,
                 opacity=1
             ).add_to(peru_map)
         else:
             folium.PolyLine(
-                [best_route[i].position, best_route[0].position],
+                [route[i].position, route[0].position],
                 color="blue",
                 weight=2.5,
                 opacity=1
             ).add_to(peru_map)
+    peru_map.save(r"peru_map.html")
 
-    peru_map.save(r"C:\Users\diego\PycharmProjects\Trabajo1\peru_map.html")
 
-    options = webdriver.ChromeOptions()
-    options.binary_location = r"C:\Program Files\Google\Chrome Beta\Application\chrome.exe"
-    chrome_driver_binary = r"C:\Users\diego\Desktop\chromedriver.exe"
-    driver = webdriver.Chrome(chrome_driver_binary, options=options)
-    driver.set_window_size(1080, 1020)  # choose a resolution
-    driver.get(r'C:\Users\diego\PycharmProjects\Trabajo1\peru_map.html')
-    # You may need to add time.sleep(seconds) here
-    driver.save_screenshot(r'C:\Users\diego\PycharmProjects\Trabajo1\peru_map.png')
-    pygame.init()
-
-    screen = pygame.display.set_mode((1080, 1020))
-    square = pygame.Surface((50, 70))
-    bg = pygame.image.load(r'C:\Users\diego\PycharmProjects\Trabajo1\peru_map.png').convert()
-    square.fill((0, 180, 250))
-    while True:
-        screen.fill((128, 255, 128))
-        screen.blit(bg, (10, 50))
-        if pygame.event.get(pygame.QUIT):
-            break
-        pygame.display.update()
-
-    pygame.quit()
+if __name__ == '__main__':
+    departments = get_departments_list()
+    best_route = simulated_annealing(departments, 10000)
+    create_map(best_route)
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,6 +1,9 @@
 import random
 import math
+import time
+import folium
 from department import Department, get_haversine_distance
+import pygame
 
 
 def get_energy(route):
@@ -9,6 +12,33 @@ def get_energy(route):
         energy += Department.get_distance_between_departments(route[i], route[i + 1])
     energy += Department.get_distance_between_departments(route[len(route)-1], route[0])
     return energy
+
+
+def create_image(route):
+    peru_map = folium.Map(location=[-9.497415, -75.142212], zoom_start=6, zoom_control=False)
+    for i in range(len(route)):
+        folium.Circle(
+            route[i].position,
+            radius=10000,
+            popup=route[i].name,
+            color="crimson",
+            fill=True,
+        ).add_to(peru_map)
+        if i < len(route) - 1:
+            folium.PolyLine(
+                [route[i].position, route[i + 1].position],
+                color="blue",
+                weight=2.5,
+                opacity=1
+            ).add_to(peru_map)
+        else:
+            folium.PolyLine(
+                [route[i].position, route[0].position],
+                color="blue",
+                weight=2.5,
+                opacity=1
+            ).add_to(peru_map)
+    peru_map.save(r"peru_map.html")
 
 
 def simulated_annealing(route, iterations):
@@ -34,11 +64,7 @@ def simulated_annealing(route, iterations):
         if e_actual < e_best:
             t_best = t_actual.copy()
         temp = (1 - cooling_index) * temp
-
-
-
-
-
+        #create_image(t_actual)
 
     print(str(get_energy(t_best)) + "km")
     return t_best
